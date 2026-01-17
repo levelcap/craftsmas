67559cdc084b59545f7b4d71bdc87cf49c0a2110
Dave Cohen <dave@bookbub.com>
2026-01-17 13:37:20 -0500

    Putting in some basic associations because I am bad at using the model generator

--- app/models/gift_exchange.rb
+++ app/models/gift_exchange.rb
@@ -1,2 +1,3 @@
 class GiftExchange < ApplicationRecord
+  has_many :users, through: :gift_exchange_users
 end
--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -1,2 +1,4 @@
 class WishlistItem < ApplicationRecord
+  belongs_to :wishlist
+  belongs_to :user, through: :wishlist
 end
